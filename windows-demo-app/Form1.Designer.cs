d4b1d2aff5632bfa3c44b4c1d7531a145c3e1a8c
Hugh (Spike) McLarty <spike.mclarty@gmail.com>
2013-07-15 21:10:22 -0400

    added drop-list for discovered devices

--- windows-demo-app/Form1.Designer.cs
+++ windows-demo-app/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.deviceList = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // deviceList
+            // 
+            this.deviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.deviceList.FormattingEnabled = true;
+            this.deviceList.Location = new System.Drawing.Point(13, 13);
+            this.deviceList.Name = "deviceList";
+            this.deviceList.Size = new System.Drawing.Size(235, 21);
+            this.deviceList.Sorted = true;
+            this.deviceList.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(339, 274);
+            this.Controls.Add(this.deviceList);
             this.Name = "Form1";
             this.Text = "Windows SWORD Demo (C#)";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox deviceList;
     }
 }
 
