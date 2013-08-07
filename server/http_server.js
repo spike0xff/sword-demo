24c8ce2d21651543fbaccf3fac882254665e2101
spike <spike.mclarty@gmail.com>
2013-08-07 08:15:47 -0400

    Mark's update, invoke driver and return HTML!

--- server/http_server.js
+++ server/http_server.js
@@ -3,19 +3,25 @@ var util = require('util');
 var spawn = require('child_process').spawn;
 
 http.createServer( function(req,res) {
-
    var currentTime = new Date();
    console.log('Client called at '+currentTime);
 
-   if (req.method == 'POST' || req.method == 'GET') {
+   // Handle POST...
+   if (req.method == 'POST') {
+
+      // Receive the POST data...
       var body = '';
       req.on('data', function(data) {
          body += data;
       });
 
+      // Handle the response...
       req.on('end', function() {
-         var child = spawn('date', []);
+         // Issue a command...
+         var child = spawn('/home/pi/cdrv/sword-demo/driver/driver', ["html"]);
          var output = '';
+
+         // Collect the data on standard in and error...
          child.stdout.on('data', function(data) {
             console.log('stdout: ' + data);
             output += data;
@@ -23,9 +29,11 @@ http.createServer( function(req,res) {
          child.stderr.on('data', function(data) {
             console.log('stderr: ' + data);
          });
+
+         // Write the response...
          child.stdout.on('end', function() {
             console.log('stdout end');
-            res.writeHead(200, "OK", {'Content-Type':'text/plain'});
+            res.writeHead(200, "OK", {'Content-Type':'text/html'});
             res.write('SWORD Scanning Server on nodejs\n');
             res.write('--------- post body:\n');
             res.write(body+'\n');
